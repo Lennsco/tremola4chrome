99b3fd39eeb97840957c11af723003444abd2227
Lennsco <leandro09.lika@gmail.com>
2025-07-01 18:55:18 +0200

    Add run.sh

---
+++ tremola4chrome/run.sh
@@ -0,0 +1,7 @@
+open -na "Google Chrome" --args \
+    "$PWD/user_alice.html" \
+    "$PWD/user_bob.html" \
+    "$PWD/user_carol.html" \
+    --allow-file-access-from-files \
+    --window-size=200,1800  
+    
